1f59cc17b13ca6170bf58d2da4f9a483c1ac1080
HL <hi.heyse@gmail.com>
2016-11-18 15:56:03 -0500

    Format number to 2 decimal places

--- app/Products/Product.js
+++ app/Products/Product.js
@@ -3,22 +3,22 @@ import { Text, View, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   wrapper: {
-    padding: 0,
+    padding: 10,
     flex: 1,
     flexDirection: 'row',
   },
   name: {
-    padding: 1,
+    padding: 0,
   },
   price: {
-    padding: 1,
+    padding: 0,
   },
 });
 
 const Product = ({ product }) => (
   <View style={styles.wrapper}>
     <Text style={styles.name}>{product.name}</Text>
-    <Text style={styles.price}>${product.price}</Text>
+    <Text style={styles.price}>${product.price ? product.price.toFixed(2) : 'nan'}</Text>
   </View>
 );
 
