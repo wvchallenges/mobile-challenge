762f89420728359a9ea12bc7d1fea592b9375c8b
HL <hi.heyse@gmail.com>
2016-11-18 16:09:55 -0500

    Revert "Refactor styles to own file"
    This reverts commit acddc742caccc5ac9c39714465db96c6b4de5601.

--- app/Products/Product.js
+++ app/Products/Product.js
@@ -1,6 +1,22 @@
 import React, { PropTypes } from 'react';
-import { Text, View } from 'react-native';
-import styles from './styles';
+import { Text, View, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  wrapper: {
+    padding: 10,
+    flex: 1,
+    flexDirection: 'row',
+  },
+  name: {
+    paddingRight: 10,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  price: {
+    padding: 0,
+    fontSize: 20,
+  },
+});
 
 const Product = ({ product }) => (
   <View style={styles.wrapper}>
