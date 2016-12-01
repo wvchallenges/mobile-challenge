6a7ab02d5e64c803f7bcb06fa1eb8bbbdac315a3
Chun Chen <chun.chen@fanta7.com>
2016-11-30 21:54:22 -0500

    Initial commit

---
+++ Wave/Wave/WProductsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  WProductsViewController.swift
+//  Wave
+//
+//  Created by cc on 16/11/30.
+//  Copyright © 2016年 Wave. All rights reserved.
+//
+
+import UIKit
+
+class WProductsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
