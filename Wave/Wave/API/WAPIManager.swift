7f0927889745fd9b5b0110d990d42cae05a78d32
Chun Chen <chun.chen@fanta7.com>
2016-12-02 23:34:47 -0500

    Implement APIManager

--- Wave/Wave/API/WAPIManager.swift
+++ Wave/Wave/API/WAPIManager.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+class WAPIManager {
+    static let sharedInstance = WAPIManager()
+    private init() {
+    }
+    
+    private func failureHandler(_ errorMessage: String) {
+        print(errorMessage)
+    }
+    
+    func getProducts(requsetParams: WProductsRequestParams, handler: @escaping(_ response: Array<WProductModel>)->Void) {
+        let apiProducts = WAPIProducts()
+        apiProducts.getProducts(requestParams: requsetParams, successHandler: {
+            response in
+            handler(response)
+        }, failureHandler: {
+            errorMessage in
+            self.failureHandler(errorMessage)
+        })
+    }
+}
