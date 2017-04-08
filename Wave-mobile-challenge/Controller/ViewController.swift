a7a317dfdab7c3690ecb2262a2b7c6519a6aaee1
John Warmann <john.warmann@gmail.com>
2017-04-08 15:09:29 -0400

    Initial project setup

---
+++ Wave-mobile-challenge/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wave-mobile-challenge
+//
+//  Created by John Warmann on 2017-04-08.
+//  Copyright © 2017 LocalizeWiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
