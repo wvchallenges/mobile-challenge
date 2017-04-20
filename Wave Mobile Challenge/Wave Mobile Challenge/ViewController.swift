90a9e01899cc9c7a9cb8755278b5a4ed590514db
Artur Shinkevich <archican91@gmail.com>
2017-04-20 16:47:01 -0400

    - Initial project commit

---
+++ Wave Mobile Challenge/Wave Mobile Challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wave Mobile Challenge
+//
+//  Created by Artur Shinkevich on 2017-04-20.
+//  Copyright © 2017 ashinkevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
