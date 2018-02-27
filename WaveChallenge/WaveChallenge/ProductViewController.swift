36834b53352373af6c9a2bbfe6720822fda6d118
Akash Patel <akash.lambton@gmail.com>
2018-02-27 00:40:52 -0500

    Initial Commit

---
+++ WaveChallenge/WaveChallenge/ProductViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WaveChallenge
+//
+//  Created by Akash Patel on 2018-02-27.
+//  Copyright © 2018 Akash Patel. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
