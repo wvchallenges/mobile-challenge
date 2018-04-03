1c0bec7e03f425444629c286355cad83e06676fd
Naweed Tajuddin <naweedtajuddin@Naweeds-MacBook-Pro.local>
2018-04-03 03:55:30 -0400

    wave challenge

---
+++ wave-mobile-challenge-ios/wave-mobile-challenge-ios/Product.swift
@@ -0,0 +1,9 @@
+//
+//  Product.swift
+//  wave-mobile-challenge-ios
+//
+//  Created by Naweed Tajuddin on 2018-04-03.
+//  Copyright © 2018 Naweed Tajuddin. All rights reserved.
+//
+
+import Foundation
