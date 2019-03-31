fb9c2a9469937b97d375e211b2c775065ebc94bd
Lalit <agarwal.lalit91@gmail.com>
2019-03-31 01:19:04 -0400

    Added MainActivity class and library dependencies.

---
+++ app/src/main/java/android/sample/wavechallenge/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package android.sample.wavechallenge.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.sample.wavechallenge.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
