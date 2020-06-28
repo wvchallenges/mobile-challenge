989549dd56859c3bfef4a4729f98df84d062567e
Steven Wu <wu.s.k.steven@gmail.com>
2020-06-27 23:43:23 -0400

    SW – Add empty API keys file

---
+++ Listings/APIKeys.swift
@@ -0,0 +1,14 @@
+//
+//  APIKeys.swift
+//  Listings
+//
+//  Created by Steven Wu on 2020-06-27.
+//  Copyright © 2020 Steven Wu. All rights reserved.
+//
+
+import Foundation
+
+struct APIKeys {
+    // TODO: SW - improve obfuscation here
+    static let productsAuthToken = ""
+}
