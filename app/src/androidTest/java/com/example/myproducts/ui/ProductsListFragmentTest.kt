f691797e5c812a86f451901e953c6c4eb4c99391
Roman Mbwasi <romanmbwasi@gmail.com>
2021-04-14 17:41:59 -0400

    Readme update

--- app/src/androidTest/java/com/example/myproducts/ui/ProductsListFragmentTest.kt
+++ app/src/androidTest/java/com/example/myproducts/ui/ProductsListFragmentTest.kt
@@ -1,21 +1,16 @@
 package com.example.myproducts.ui
 
-import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Espresso.pressBack
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.example.myproducts.MainActivity
 import com.example.myproducts.R
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 
 class ProductsListFragmentTest{
-
     //Launches activity before each test
     @get:Rule
     val mainActivityRule = ActivityScenarioRule(MainActivity::class.java)
