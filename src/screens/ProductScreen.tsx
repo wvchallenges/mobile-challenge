74e923b65947236464ba014350c4f00effe7d603
Spring <development.spring@gmail.com>
2021-10-29 00:48:47 +0800

    show products

--- src/screens/ProductScreen.tsx
+++ src/screens/ProductScreen.tsx
@@ -1,48 +1,81 @@
-import React from "react";
-import { FlatList, SafeAreaView, StatusBar, Text, View } from "react-native";
-import useSWR from "swr";
-import { productFetcher } from "../api/api";
-import { Product } from "../api/models/Product";
+import React from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  SafeAreaView,
+  StatusBar,
+  Text,
+  View,
+} from 'react-native';
+import useSWR from 'swr';
+import {productFetcher} from '../api/api';
+import {Product} from '../api/models/Product';
 
 const ProductScreen = () => {
+  const {data, error} = useSWR(
+    ['/products/', '89746d57-c25f-4cec-9c63-34d7780b044b'],
+    productFetcher,
+  );
 
-    const { data } = useSWR(['/products/', '89746d57-c25f-4cec-9c63-34d7780b044b'], productFetcher)
-
-    // const data = [{
-    //     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    //     title: 'hello world',
-    // },{
-    //     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28bb',
-    //     title: 'hello world',
-    // },{
-    //     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28bc',
-    //     title: 'hello world',
-    // }]
-
-    const renderItem = ({
-        item
-    }: {
-        item: Product
-    }) => (
-        <View style={{
+  const renderItem = ({item}: {item: Product}) => (
+    <View
+      style={{
+        flexDirection: 'column',
+        backgroundColor: 'cornflowerblue',
+        borderRadius: 8,
+        marginTop: 8,
+        padding: 16,
+      }}>
+      <View
+        style={{
+          flexDirection: 'row',
         }}>
-            <Text style={{
-                fontSize: 24,
+        <Text
+          style={{
+            fontSize: 24,
+            flex: 1,
+          }}>
+          {item.name}
+        </Text>
+        <View
+          style={{
+            borderRadius: 4,
+            backgroundColor: 'orange',
+            padding: 4,
+          }}>
+          <Text
+            style={{
+              fontSize: 20,
             }}>
-                {item.name}
-            </Text>
+            {`$${item.price}`}
+          </Text>
         </View>
-      );
-
-    return (
-        <SafeAreaView style={{
-            flexDirection: "column",
+      </View>
+      <Text
+        style={{
+          fontSize: 20,
         }}>
-          <StatusBar  />
-          <FlatList style={{
-          }} data={data ?? []} renderItem={renderItem} />
-        </SafeAreaView>
-      );
-}
+        {item.description}
+      </Text>
+    </View>
+  );
+
+  return (
+    <SafeAreaView
+      style={{
+        flexDirection: 'column',
+      }}>
+      <StatusBar />
+      {!data && !error ? <ActivityIndicator size="large" /> : null}
+      <FlatList
+        style={{
+          paddingHorizontal: 8,
+        }}
+        data={data ?? []}
+        renderItem={renderItem}
+      />
+    </SafeAreaView>
+  );
+};
 
 export default ProductScreen;
