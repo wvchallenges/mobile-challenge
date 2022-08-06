1b10bfa1579f698d4a68cf092ae8df16e71d1884
Fadia <69068209+fadiaIW@users.noreply.github.com>
2022-08-06 11:04:33 -0400

    Revert "Screens, API integration"
    This reverts commit b7af3b5cceb4a6a7a478d14fd8be88e90541eac2.

--- ios/Pods/Target Support Files/Pods-Wave-WaveTests/Pods-Wave-WaveTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Wave_WaveTests : NSObject
-@end
-@implementation PodsDummy_Pods_Wave_WaveTests
-@end
--- src/components/colors.js
+++
@@ -1,8 +0,0 @@
-export const COLORS = {
-  bgColor: '#efefef',
-  borderColor: '#bebebe',
-  greyText: '#6e6e6e',
-  detailGrey: '#242526',
-  headerText: '#2D2D2D',
-  red: '#B22222',
-};
